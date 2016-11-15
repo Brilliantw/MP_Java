550b1367a105b3cece8fa28e41b2c5492fb2ee8f
lab <mjw95@kookmin.ac.kr>
2016-11-15 14:01:37 +0900

    BankAccount Quickcode commit

---
+++ src/Quick05/testMain.java
@@ -0,0 +1,26 @@
+package Quick05;
+
+public class testMain {
+
+		public static BankAccount account = new BankAccount();
+		
+		public static void main (String[] args){
+			
+			Thread depositMan = new Thread() {
+				public void run(){
+					account.add(10);
+				}
+				
+				
+			};
+			Thread withdrawMan = new Thread() {
+				public void run(){
+					account.delete(10);
+				}
+			};
+			depositMan.start();
+			withdrawMan.start();
+		}
+}
+
+
